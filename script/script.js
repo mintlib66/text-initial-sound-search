4e8cefa376fb75e983af116fc9acd74c7207c688
hy <mintlib66@gmail.com>
2022-04-23 20:18:08 +0900

    -검색 후 개행 없어지는 오류 수정 -검색 후 화면에서 돌아가는 기능 추가

--- script/script.js
+++ script/script.js
@@ -22,51 +22,86 @@ $(function () {
     { pos: 18, sound: 'ㅎ', select: false },
   ]
   const HANGUL_CHAR_NUM = 588
+  let selected = false
 
-  //레이아웃, 초기값 설정
-  initialSounds.map(item => {
-    $('.setting-box').append(`<li class='initial-sound'>${item.sound}</li>`)
-    // console.log(`${item.sound}: ${item.start.charCodeAt(0) + 587}`)
-  })
-  $('.information').text(
-    `*사용방법: 초성 글자 버튼 클릭 후, 검색을 원하는 텍스트를 아래
-    입력칸에 적은 후, 검색 버튼을 누릅니다.`
-  )
-  $('.textInput').val(
-    `가까나다따라마바빠사싸아자짜차카타파하\n데이터 속성은 순 HTML 속성이기 때문에 CSS에서도 접근할 수 있다는 것에 주목하세요.`
-  )
+  initialize()
 
-  // 버튼 선택상태 확인
-  $('.setting-box').on('click', e => {
-    e.target.classList.toggle('clicked')
-    const selectItem = e.target.innerText
+  function initialize() {
+    $('main').html(
+      `<p class="information"></p>
+      <ul class="setting-box"></ul>
+      <textarea
+      class="textInput"
+      placeholder="검색할 텍스트를 입력해주세요."
+      ></textarea>`
+    )
+
+    //레이아웃, 초기값 설정
     initialSounds.map(item => {
-      if (item.sound === selectItem) {
-        item.select = item.select === false ? true : false
-      }
+      $('.setting-box').append(`<li class='initial-sound'>${item.sound}</li>`)
     })
-  })
+    $('.information').text(
+      `*사용방법: 초성 글자 버튼 클릭 후, 검색을 원하는 텍스트를 아래
+    입력칸에 적은 후, 검색 버튼을 누릅니다.`
+    )
+    // $('.textInput').val(
+    //   `가까나다따라마바빠사싸아자짜차카타파하\n데이터 속성은 순 HTML 속성이기 때문에 CSS에서도 접근할 수 있다는 것에 주목하세요.`
+    // )
+    // 버튼 선택상태 확인
+    $('.setting-box').on('click', e => {
+      e.target.classList.toggle('clicked')
+      const selectItem = e.target.innerText
+      initialSounds.map(item => {
+        if (item.sound === selectItem) {
+          item.select = item.select === false ? true : false
+        }
+      })
+    })
+  }
 
   // 검사 버튼 클릭 이벤트 핸들러 (실행 후 버튼 타입변경)
   $('.btn').on('click', e => {
-    if (e.target.dataset.type === 'find') {
-      const code = searchText($('.textInput').val())
-      $('.main-box').html(code)
+    if ($(e.target).data('type') === 'find') {
+      handleFindBtnClick($('.textInput').val(), $(e.target))
+    } else if ($(e.target).data('type') === 'back') {
+      initialize()
+      initializeSoundSelect()
+      $(e.target).data('type', 'find')
+      $(e.target).text('검색')
     }
   })
 
-  // 검사 실행
-  function searchText(text) {
-    const textArr = [...text]
+  //select상태 초기화
+  function initializeSoundSelect() {
+    initialSounds.map(item => {
+      item.select = false
+    })
+  }
 
+  //검색 버튼 컨트롤
+  function handleFindBtnClick(text, btn) {
+    const textArr = [...text]
     const filteredSounds = initialSounds.filter(
       initialSound => initialSound.select === true
     )
-    console.log(filteredSounds)
+    // console.log(textArr)
 
-    const searchTextArr = textArr.reduce((para, textItem) => {
-      //공백 제외
-      if (textItem === ' ') return para + textItem
+    if (filteredSounds.length > 0) {
+      const code = findText(textArr, filteredSounds)
+      $('main').html(code)
+      // console.log($('main').html())
+      btn.data('type', 'back')
+      btn.text('돌아가기')
+    } else {
+      alert('하나 이상의 초성을 선택해주세요.')
+    }
+  }
+
+  // 검색 실행
+  function findText(textArr, filteredSounds) {
+    const findTextArr = textArr.reduce((para, textItem) => {
+      //개행 처리
+      if (textItem === '\n') return para + '<br/>'
 
       const textCode = textItem.charCodeAt(0) - 44032
 
@@ -78,14 +113,13 @@ $(function () {
         let result = textItem
         filteredSounds.find(item => {
           if (item.pos === getPos(textCode)) {
-            console.log(textItem)
             result = `<strong class="char-${item.pos}">${textItem}</strong>`
           }
         })
         return para + result
       }
     }, '<p class="textResult">')
-    return searchTextArr + '</p>'
+    return findTextArr + '</p>'
   }
 
   function getPos(code) {
